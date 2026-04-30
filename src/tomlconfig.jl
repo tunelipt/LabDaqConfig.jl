7f56323678cbde7fa0f1402feec196470c297ed9
= <pjabardo@ipt.br>
2026-04-30 10:22:24 -0300

    Changed the name from objects.jl to tomlconfig.jl
    I forgot that I had started working on this and didn't notice the file.

--- src/tomlconfig.jl
+++ src/tomlconfig.jl
@@ -1,19 +1,24 @@
+# Here we provide the basic framework to build obects from TOML configurations
 
-export tomlconfig
+const _labdaqtypes_ = Dict{String,Function}()
 
+function labdaqcreate(toml, field="type")
+    # This is the basic interface!
+    k = keys(toml)
 
-const __tomlconfig_modules__ = Dict{String,Any}()
-
-"""
-`tomlconfig()`
-
-A function that is used to create objects from TOML configuration files.
-
-The TOML dictionary should have an `type` field that 
-
-"""
-
-function tomlconfig(toml)
+    if field ∉ k
+        error("The configuration file should have field $field!")
+    end
     
+    t = toml[field]
+    if t ∉ keys(_labdaqtypes_)
+        error("Type $t is not registered in `LabDaqConf`")
+    end
+
+    return _labdaqtypes_[t](toml)
+end
+
+function labdaqregister(ftype::String, fun::Function)
+    _labdaqtypes_[ftype] = fun
 end
 
